9617e1e25bbb56545f985af44bd660c98ab9d8fd
Adiks <apacholak97@interia.pl>
2022-05-06 09:51:24 +0200

    primary beans and profiles

--- src/main/java/com/adrian/sfgdi/services/PrimaryGreetingService.java
+++ src/main/java/com/adrian/sfgdi/services/PrimaryGreetingService.java
@@ -1,10 +1,5 @@
 package com.adrian.sfgdi.services;
 
-import org.springframework.context.annotation.Primary;
-import org.springframework.stereotype.Service;
-
-@Service
-@Primary
 public class PrimaryGreetingService implements GreetingService {
 
     @Override
